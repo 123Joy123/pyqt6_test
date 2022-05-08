e4064878c5e13bafd421f31f3c3a19a09f6fb27e
123Joy123 <2506114287@qq.com>
2022-05-08 16:14:12 +0800

    a few setting

--- WindowClass.py
+++ WindowClass.py
@@ -1,11 +1,28 @@
 from PyQt6.QtWidgets import QApplication, QWidget
 import sys
-
-from numpy import append
+from PyQt6.QtGui import QIcon
 
 class Window(QWidget):
     def __init__(self):
         super().__init__()
+         # 设置标题
+        self.setWindowTitle("PyQt6 Window")
+
+        # 设置固定宽高
+        # self.setFixedHeight(500)
+        # self.setFixedWidth(500)
+        self.setGeometry(500, 300, 400, 300) # 屏幕显示位置x，y值，和宽高
+
+        # 设置背景颜色
+        # self.setStyleSheet("background-color:red")
+        stylesheet = (
+            "background-color:red"
+        )
+        self.setStyleSheet(stylesheet)
+
+
+
+
 
 app = QApplication(sys.argv)
 window = Window()
